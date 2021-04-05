fa9baaa812402affed8b2e91c6076239eb8f515f
DmitrTRC <9984398@gmail.com>
2021-04-05 23:24:28 +0300

    for loop started. Not completed yet.

--- words_brackets.cpp
+++ words_brackets.cpp
@@ -7,5 +7,17 @@
 using namespace std;
 
 int main(){
+    string query;
+    string target_str ="[";
+    getline(cin, query);
+
+    for ( auto index = 0; index < query.length(); index++){
+        if ( query[index] != ' '){
+            target_str += query[index];
+        }else{
+            target_str += "]";
+        }
+    }
+    cout << target_str;
 
 }
