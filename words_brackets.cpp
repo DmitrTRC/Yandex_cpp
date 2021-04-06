377e847f11d052bbcc1c7c5146bdf9a21d34291f
DmitrTRC <9984398@gmail.com>
2021-04-06 20:15:33 +0300

    get_next_word , main() : Done.

--- words_brackets.cpp
+++ words_brackets.cpp
@@ -9,28 +9,37 @@ using namespace std;
 
 bool get_next_word(const string &source_string, string::size_type &head, string &result_string) {
     bool result = false;
+    result_string = "";
     if (head < source_string.length()) {
-
-        result_string.push_back(source_string[head++]);
+        if (isspace(source_string[head])){
+            head ++;
+        } else {
+        result_string.push_back(source_string[head++]);}
         for (; head < source_string.length(); head++) {
             if (not isspace(source_string[head])) {
                 result_string.push_back(source_string[head]);
 
             } else break;
+
         }
         result = true;
     }
     return result;
 }
 
+string print_word(const string &word) {
+    return "[" + word + "]";
+}
+
 int main() {
     string query;
-    string target_str = "";
     getline(cin, query);
     string::size_type head(0);
     string next_word;
-    get_next_word(query, head, next_word);
-    cout << next_word;
-
-    cout << target_str;
+    auto count(0);
+    while (get_next_word(query, head, next_word)) {
+        count++;
+        cout << print_word(next_word);
+    }
+    cout << endl;
 }
