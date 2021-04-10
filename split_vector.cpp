43369669d4f25741635947b4036f518ee49c9ba6
DmitrTRC <9984398@gmail.com>
2021-04-10 03:16:32 +0300

    split()

--- split_vector.cpp
+++ split_vector.cpp
@@ -9,23 +9,24 @@ using namespace std;
 
 void split(const string &source_string, vector<string> &words) {
     string word;
-    for (auto(letter) : source_string) {
-        if (letter == ' ') {
+    for (int i =0; i < source_string.size(); i++) {
+        if (source_string[i] == ' ') {
             words.push_back(word);
             word.clear();
         } else {
-            word += letter;
+            word += source_string[i];
         }
     }
 }
 
-void print_list(const vector<string> list) {
-    for (auto(item) : list) {
+void print_list(vector<string> &list) {
+    for (const auto(&item) : list) {
         cout << "[" << item << "]" << endl;
     }
 }
 
 int main() {
+
     string query;
     getline(cin, query);
     vector<string> words;
