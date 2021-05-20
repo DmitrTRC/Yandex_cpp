bf3c4e8e011acfbdbf736439e6b6b06aef0c0592
Dmitry <9984398@gmail.com>
2021-05-21 00:29:02 +0200

    Factorial: Done

--- srs/functions/factorial.cpp
+++ srs/functions/factorial.cpp
@@ -1,10 +1,17 @@
 //
 // Created by Dmitry Morozov on 20/5/21.
 //
+#include <iostream>
+
 int Factorial(int x) {
-    // здесь вычисляем факториал числа и возвращаем полученное значение
+    if (x <= 0) return 1;
+    else return x * Factorial(x - 1);
+
 }
 
 int main() {
+    int n;
+    std::cin >> n;
+    std::cout << Factorial(n) << std::endl;
 }
 
