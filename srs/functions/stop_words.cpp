949ab169f0bb397a10528f4a57246c6896c59d63
Dmitry <9984398@gmail.com>
2021-05-21 14:15:19 +0200

    Stop_words. Start to write filter

---
+++ srs/functions/stop_words.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Dmitry Morozov on 21/5/21.
+//
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> SplitIntoWords(std::string line, const std::string &delimiter = " ") {
+    vector<string> ret_line;
+    size_t pos = 0;
+    std::string token;
+    while ((pos = line.find(delimiter)) != std::string::npos) {
+        token = line.substr(0, pos);
+        ret_line.push_back(token);
+        line.erase(0, pos + delimiter.length());
+    }
+    ret_line.push_back(line);
+    return ret_line;
+}
+
+int main() {
+    string query;
+    getline(cin, query);
+
+    for (string word : SplitIntoWords(query)) {
+        cout << '[' << word << ']' << endl;
+    }
+}
