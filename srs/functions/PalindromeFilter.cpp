53e278e0ca9812701b28fc47fcfe18421acc1339
Dmitry <9984398@gmail.com>
2021-05-21 08:17:03 +0200

    IsPalindrome() : Added.

--- srs/functions/PalindromeFilter.cpp
+++ srs/functions/PalindromeFilter.cpp
@@ -1,4 +1,13 @@
 //
 // Created by Dmitry Morozov on 21/5/21.
 //
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+void IsPalindrome(string s) {
+    int answer{0};
+    if (s == string(s.rbegin(), s.rend())) answer = 1;
+    cout << answer << endl;
+}
