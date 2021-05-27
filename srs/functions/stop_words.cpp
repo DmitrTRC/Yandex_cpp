6e1720b8d0428253f701143c29f933db20825824
Dmitry Morozov <morozovd@librem.one>
2021-05-27 08:16:59 +0200

    Testing: 29 / 33 Passed. Refactored.
    Возможные проблемы:
    - Вы неправильно обрабатываете случай, когда часть слов исключена из запроса

--- srs/functions/stop_words.cpp
+++ srs/functions/stop_words.cpp
@@ -1,46 +1,49 @@
 //
 // Created by Dmitry Morozov on 21/5/21.
 //
+
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 #include <set>
-#include <algorithm>
+#include <sstream>
+#include <iterator>
+
 
 using namespace std;
 
-vector<string> SplitIntoWords(std::string line, const std::string &delimiter = " ") {
-    vector<string> ret_line;
-    size_t pos = 0;
-    std::string token;
-    while ((pos = line.find(delimiter)) != std::string::npos) {
-        token = line.substr(0, pos);
-        ret_line.push_back(token);
-        line.erase(0, pos + delimiter.length());
-    }
-    ret_line.push_back(line);
-    return ret_line;
+
+vector<string> SplitIntoWords(const std::string &text) {
+    std::istringstream iss(text);
+    std::vector<std::string> results(std::istream_iterator<std::string>{iss},
+                                     std::istream_iterator<std::string>());
+    return results;
+
 }
 
-vector<string> StopWordsFilter(vector<string> stop_srs, vector<string> request) {
-    set<string> stop_set(stop_srs.begin(), stop_srs.end());
-    set<string> request_set(request.begin(), request.end());
-    vector<string> result_vec;
-    set_difference (request.begin(), request.end(), stop_set.begin(), stop_set.end(), back_inserter(result_vec));
+std::set<string> NormalizeStopWords(const std::string &srs_words) {
+    std::set<string> result_set;
+    for (const auto &word : SplitIntoWords(srs_words)) result_set.insert(word);
+    return result_set;
+}
+
+vector<string> FilterQuery(const std::string &query, const std::set<string> &stop_words) {
+    std::vector<string> result_vec;
+    for (const auto &word : SplitIntoWords(query)) {
+        if (!stop_words.count(word)) result_vec.push_back(word);
+    }
     return result_vec;
 }
 
+
 int main() {
-    string stop_words;
+    string stop_words_string;
     string query;
 
-    getline(cin, stop_words);
+    getline(cin, stop_words_string);
     getline(cin, query);
 
-    vector<string> result = StopWordsFilter(SplitIntoWords(stop_words), SplitIntoWords(query));
-
-    for (const auto &item: result) {
+    for (auto const &item : FilterQuery(query, NormalizeStopWords(stop_words_string))) {
         cout << "[" << item << "]" << endl;
     }
-
 }
