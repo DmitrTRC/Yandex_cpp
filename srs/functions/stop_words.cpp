864f52c0f6290f56c70d33e9c755cc9d78430708
Dmitry Morozov <morozovd@librem.one>
2021-05-27 08:29:19 +0200

    Testing: 29 / 33 Passed.  Problem localized in SplitIntoWords()
    Возможные проблемы:
    - Вы неправильно обрабатываете случай, когда часть слов исключена из запроса

--- srs/functions/stop_words.cpp
+++ srs/functions/stop_words.cpp
@@ -13,14 +13,39 @@
 using namespace std;
 
 
-vector<string> SplitIntoWords(const std::string &text) {
-    std::istringstream iss(text);
-    std::vector<std::string> results(std::istream_iterator<std::string>{iss},
-                                     std::istream_iterator<std::string>());
-    return results;
+//vector<string> SplitIntoWords(const std::string text) {
+//    std::istringstream iss(text);
+//    std::vector<std::string> results(std::istream_iterator<std::string>{iss},
+//                                     std::istream_iterator<std::string>());
+//    return results;
+//}
 
+//vector<string> SplitIntoWords(string text) {
+//    vector<string> words;
+//    string word;
+//    for (int i = 0; i < text.size(); ++i) {
+//        if (text[i] == ' ') {
+//            words.push_back(word);
+//            word = ""s;
+//        } else {
+//            word += text[i];
+//        }
+//    }
+//    words.push_back(word);
+//
+//    return words;
+//}
+std::vector<std::string> SplitIntoWords(const std::string& s, char delimiter = ' ')
+{
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(s);
+    while (std::getline(tokenStream, token, delimiter))
+    {
+        tokens.push_back(token);
+    }
+    return tokens;
 }
-
 std::set<string> NormalizeStopWords(const std::string &srs_words) {
     std::set<string> result_set;
     for (const auto &word : SplitIntoWords(srs_words)) result_set.insert(word);
