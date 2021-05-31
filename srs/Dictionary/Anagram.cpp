a92fdf71b0c55932df7c48639bdceb15330efbbd
Dmitry <9984398@gmail.com>
2021-05-31 22:27:53 +0200

    Anagram.cpp : Start.

---
+++ srs/Dictionary/Anagram.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Dmitry Morozov on 31/5/21.
+//
+
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        string first_word, second_word;
+        cin >> first_word >> second_word;
+        if (...) {
+            cout << "YES"s << endl;
+        } else {
+            cout << "NO"s << endl;
+        }
+    }
+
+    return 0;
+}
