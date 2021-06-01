201c3659d90f14eed428443772a8a388f18fcc6a
Dmitry <9984398@gmail.com>
2021-06-01 22:15:58 +0200

    Birds: Template loaded. Work. Not tested.

--- srs/Dictionary/Birds1.cpp
+++ srs/Dictionary/Birds1.cpp
@@ -4,12 +4,21 @@
 
 #include <string>
 #include <vector>
+#include <map>
+#include<iostream>
 
 using namespace std;
 
 
 void PrintBirdNumByType(const vector<string> &types) {
-    // напишите здесь ваше решение
+    map<string, int> result_map;
+    for (const auto &BirdType : types) {
+        ++result_map[BirdType];
+    }
+    for (const auto &[bird_type, counter] : result_map) {
+        cout << bird_type <<" "<< counter << endl;
+    }
+
 }
 
 int main() {
