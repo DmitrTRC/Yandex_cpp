73d5fbc4d76e867291f38136d370700425dcea6e
Dmitry <9984398@gmail.com>
2021-06-01 19:43:34 +0200

    Birds: Template loaded.

--- srs/Dictionary/Birds1.cpp
+++ srs/Dictionary/Birds1.cpp
@@ -1,6 +1,18 @@
 //
 // Created by Dmitry Morozov on 1/6/21.
 //
-int main(){
-    return 0;
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+void PrintBirdNumByType(const vector<string> &types) {
+    // напишите здесь ваше решение
+}
+
+int main() {
+    vector<string> bird_types = {"zyablik"s, "vorobey"s, "zyablik"s, "sinica"s, "sinica"s};
+    PrintBirdNumByType(bird_types);
 }
