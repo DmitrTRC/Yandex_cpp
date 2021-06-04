45aee87d42d2a968a0d5df507096e760ca1a103e
Dmitry <9984398@gmail.com>
2021-06-04 18:10:58 +0200

    A_Counter: Completed. TEsted.

--- srs/Algorithms/A_counter.cpp
+++ srs/Algorithms/A_counter.cpp
@@ -5,11 +5,13 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int CountStartsWithA(const vector<string>& xs) {
-    // посчитайте число строк, начинающихся на букву A
+int CountStartsWithA(const vector<string> &xs) {
+    //TODO  посчитайте число строк, начинающихся на букву A
+    return count_if(xs.begin(), xs.end(), [](string word) { return tolower(word.at(0)) == 'a'; });
 }
 
 int main() {
