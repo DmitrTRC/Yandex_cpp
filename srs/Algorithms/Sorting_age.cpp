f6e0581ba09761882730d5dc8be85acf0f5a5826
Dmitry <9984398@gmail.com>
2021-06-05 00:04:43 +0200

    Sorting_age.cpp: Completed. Not tested.

---
+++ srs/Algorithms/Sorting_age.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Dmitry Morozov on 4/6/21.
+//
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+typedef std::pair<std::string, int> p_pair;
+
+int main() {
+    int people_number{0};
+    std::vector<p_pair> personal_base;
+    for (int i = 0; i < people_number; ++i) {
+        std::string name;
+        int age;
+        std::cin >> name >> age;
+        personal_base.emplace_back(std::make_pair(name, age));
+        sort(personal_base.begin(), personal_base.end(), [](const p_pair &l, const p_pair &r) {
+                 if (l.second != r.second) {
+                     return l.second < r.second;
+                 }
+                 return l.first < r.first;
+             }
+        );
+//        reverse(personal_base.begin(), personal_base.end());
+    }
+    for (const auto &[Name, Age]: personal_base) {
+        std::cout << Name << " " << Age << std::endl;
+    }
+    return 0;
+}
