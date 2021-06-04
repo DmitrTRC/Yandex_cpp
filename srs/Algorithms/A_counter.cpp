d9835c740a34ea2de7f9e81ce748ab90ba933dd1
Dmitry <9984398@gmail.com>
2021-06-04 18:01:42 +0200

    A_Counter: Started. Template loaded.

---
+++ srs/Algorithms/A_counter.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Dmitry Morozov on 4/6/21.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int CountStartsWithA(const vector<string>& xs) {
+    // посчитайте число строк, начинающихся на букву A
+}
+
+int main() {
+    // не меняйте тело main
+    cout << CountStartsWithA({"And"s, "another"s, "one"s, "gone"s, "another"s, "one"s "bites"s, "the"s, "dust"s});
+    return 0;
+}
--- srs/Algorithms/Lesson_1_1.cpp
+++ srs/Algorithms/Lesson_1_1.cpp
@@ -7,14 +7,9 @@
 
 using namespace std;
 
-void PrintWorryCount(const vector<bool>& is_nervous) {
+void PrintWorryCount(const vector<bool> &is_nervous) {
     // подсчитываем количество элементов в векторе is_nervous, равных true
-    int worry_count = 0;
-    for (bool status : is_nervous) {
-        if (status) {
-            ++worry_count;
-        }
-    }
+    int worry_count = count(is_nervous.begin(), is_nervous.end(), true);
     cout << worry_count << endl;
 }
 
