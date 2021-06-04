eae45810920e75e8ac66762f09c4a9f3c9edba20
Dmitry <9984398@gmail.com>
2021-06-05 00:25:24 +0200

    Medianna.cpp : Started. Template loaded.

---
+++ srs/Algorithms/Medianna.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Dmitry Morozov on 5/6/21.
+//
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+pair<bool, double> CalcMedian(vector<double> samples) {
+    // TODO Medianna
+    // верните {true, медиана}, если она существует,
+    // то есть вектор непустой,
+    // иначе - {false, 0}
+    // обратите внимание - вектор принимаем по значению,
+    // так как его придётся немного подпортить, чтобы вернуть ответ
+}
+
+int main() {
+    int size;
+    cin >> size;
+
+    vector<double> samples;
+    for (int i = 0; i < size; ++i) {
+        double sample;
+        cin >> sample;
+        samples.push_back(sample);
+    }
+
+    pair<bool, double> result = CalcMedian(samples);
+    if (result.first) {
+        cout << result.second << endl;
+    } else {
+        cout << "Empty vector"s << endl;
+    }
+}
