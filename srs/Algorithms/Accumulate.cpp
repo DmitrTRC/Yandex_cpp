55b874d98474a9fb1e6433b9cb665ab9245c9e73
Dmitry <9984398@gmail.com>
2021-06-06 00:50:32 +0200

    Accumulate.cpp: started. Template loaded.

---
+++ srs/Algorithms/Accumulate.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Dmitry Morozov on 6/6/21.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+double Average(const vector<int>& xs) {
+    // посчитайте среднее арифметическое: поделите сумму на размер
+    // суммирование начните с 0.0 - тогда результат выражения будет вещественным числом
+}
+
+int main() {
+    // не меняйте код main
+    cout << Average({1, 2, 2}) << endl;
+    return 0;
+}
