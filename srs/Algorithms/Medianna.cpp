4db9ebc6098ad15ed71162d2c2bf41077424c4ba
Dmitry <9984398@gmail.com>
2021-06-05 23:05:58 +0200

    Medianna.cpp : DONE NOT TESTED.

--- srs/Algorithms/Medianna.cpp
+++ srs/Algorithms/Medianna.cpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include <utility>
 
 using namespace std;
 
@@ -15,6 +17,17 @@ pair<bool, double> CalcMedian(vector<double> samples) {
     // иначе - {false, 0}
     // обратите внимание - вектор принимаем по значению,
     // так как его придётся немного подпортить, чтобы вернуть ответ
+    pair<bool, double> result{false, 0.0};
+    if (!samples.empty()) {
+        sort(samples.begin(), samples.end());
+        double vec_center = samples.size() / 2;
+        if (samples.size() % 2) {
+            result = make_pair(true, floor(vec_center));
+        } else {
+            result = make_pair(true, (samples.at(floor(vec_center)) + samples.at(ceil(vec_center))) / 2.0);
+        }
+    }
+    return result;
 }
 
 int main() {
