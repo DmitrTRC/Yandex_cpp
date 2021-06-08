74da66327dd93c10158d5e90e39e360651c8c6eb
Dmitry <9984398@gmail.com>
2021-06-08 12:08:38 +0200

    CountNames.cpp Done. Not tested yet.

--- srs/Algorithms/CountNames.cpp
+++ srs/Algorithms/CountNames.cpp
@@ -13,12 +13,30 @@ using namespace std;
 
 const int SAMPLE_COUNT = 5;
 
-vector<int> CountNamesLong(const set<string>& storage, const vector<string>& candidates) {
+//TODO
+//Напишите функцию CountNames. Пусть она выясняет, какие слова из списка содержатся во множестве,
+// и возвращает ответ в виде вектора единиц и нулей.
+// Список кандидатов дан в виде вектора строк. Если слово есть во множестве, в ответе будет единица, иначе — ноль.
+// В коде вы видите библиотеку <chrono>. Она содержит элементы, связанные с концептами времени.
+
+// TODO Warnings.
+vector<int> CountNamesLong(const set<string> &storage, const vector<string> &candidates) {
     vector<int> output;
-    for (auto& name: candidates) {
+    output.reserve(candidates.size());
+    for (auto &name: candidates) {
         output.push_back(count(storage.begin(), storage.end(), name));
     }
     return output;
+}
+
+vector<unsigned> CountNames(const set<string> &storage, const vector<string> &candidates) {
+    vector<unsigned> result_vec;
+    result_vec.reserve(candidates.size());
+    for (auto &name: candidates) {
+        result_vec.push_back(count(storage.begin(), storage.end(), name));
+    }
+    return result_vec;
+
 }
 
 int main() {
@@ -40,8 +58,14 @@ int main() {
     cout << "Testing slow version" << endl;
     for (int i = 0; i < SAMPLE_COUNT; ++i) {
         auto begin = chrono::steady_clock::now();
-        CountNamesLong(s, v);
+        for (auto Item : CountNamesLong(s, v)) {
+            cout << Item << " ";
+        }
+        cout << endl;
+
+        // cout << CountNamesLong(s, v);
         auto end = chrono::steady_clock::now();
-        cout << "Time difference Long = "s << chrono::duration_cast<chrono::microseconds>(end - begin).count() << "[µs]"s << endl;
+        cout << "Time difference Long = "s << chrono::duration_cast<chrono::microseconds>(end - begin).count()
+             << "[µs]"s << endl;
     }
 }
